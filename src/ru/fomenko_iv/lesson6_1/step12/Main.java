55f08e39cb3a4dffc6fb07b4fb1bee345879c5f7
FomeIV <harryfom@gmail.com>
2016-03-25 08:45:07 +0300

    6.1 Generics – Шаг 12 Begin

---
+++ src/ru/fomenko_iv/lesson6_1/step12/Main.java
@@ -0,0 +1,27 @@
+package ru.fomenko_iv.lesson6_1.step12;
+
+/*
+Реализуйте generic-класс Pair, похожий на Optional, но содержащий пару элементов разных типов и не запрещающий
+элементам принимать значение null.
+
+Реализуйте методы getFirst(), getSecond(), equals() и hashCode(), а также статический фабричный метод of().
+Конструктор должен быть закрытым (private).
+
+С корректно реализованным классом Pair должен компилироваться и успешно работать следующий код:
+
+Pair<Integer, String> pair = Pair.of(1, "hello");
+Integer i = pair.getFirst(); // 1
+String s = pair.getSecond(); // "hello"
+
+Pair<Integer, String> pair2 = Pair.of(1, "hello");
+boolean mustBeTrue = pair.equals(pair2); // true!
+boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
+
+
+Пожалуйста, не меняйте модификатор доступа класса Pair. Для корректной проверки класс должен иметь пакетную видимость.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("6.1 Generics – Шаг 12");
+    }
+}
